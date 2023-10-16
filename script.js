189806bd095d47a40382e958b25c441295ec406f
Matias Britez <89950148+matibritez@users.noreply.github.com>
2023-10-16 15:46:08 -0300

    Home

---
+++ script.js
@@ -0,0 +1,104 @@
+// Definir un array con información para las tarjetas
+const datosTarjetas = [
+    {
+        imagenSrc: './img/lagoCaviahue.png',
+        imagenAlt: 'Imagen 1',
+        texto: 'Senderismo en la Montaña'
+    },
+    {
+        imagenSrc: './img/bici.png',
+        imagenAlt: 'Imagen 2',
+        texto: 'Tour en Bici'
+    },
+    {
+        imagenSrc: './img/ski.png',
+        imagenAlt: 'Imagen 3',
+        texto: 'Caviahue Ski Resort'
+    }
+];
+
+//una referencia al contenedor de tarjetas en el DOM
+const tarjetaContainer = document.getElementById('tarjeta-container');
+
+// Función para crear tarjetas desde el array
+function crearTarjetasDesdeArray() {
+    // Iterar a través del array de datos de tarjetas
+    datosTarjetas.forEach((tarjetaData) => {
+        // Crear un elemento div para la tarjeta
+        const nuevaTarjeta = document.createElement('div');
+        nuevaTarjeta.classList.add('tarjeta'); 
+
+        // Crear un elemento de imagen
+        const imagen = document.createElement('img');
+        imagen.src = tarjetaData.imagenSrc; // Ruta de la imagen desde el array
+        imagen.alt = tarjetaData.imagenAlt;
+
+        // Crear un elemento de parrafo para el texto
+        const texto = document.createElement('p');
+        texto.textContent = tarjetaData.texto; // Texto desde el array
+
+        // Agregar la imagen y el texto a la tarjeta
+        nuevaTarjeta.appendChild(imagen);
+        nuevaTarjeta.appendChild(texto);
+
+        // Agregar la tarjeta al contenedor
+        tarjetaContainer.appendChild(nuevaTarjeta);
+    });
+}
+
+// Llama a la función para crear las tarjetas al cargar la página
+crearTarjetasDesdeArray();
+
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Datos de experiencias (puedes cargar estos datos desde un servidor o una fuente externa)
+    const experienciasData = [
+        { titulo: "Laguna Hualcapen", imagen: "./img/lagunaHualcapen.png" },
+        { titulo: "Cascadas del Agrio", imagen: "./img/cascadasAgrio.png    " },
+        { titulo: "Volcan Copahue", imagen: "./img/volcanCopahuee.png" },
+        { titulo: "Salto del Agrio", imagen: "./img/saltoagrio.png" }
+    ];
+
+    // Obtener el contenedor de experiencias
+    const experienciasContainer = document.getElementById("experiencias-container");
+
+    // Crear tarjetas de experiencias dinamicamente
+    experienciasData.forEach(experiencia => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const imagen = document.createElement("img");
+        imagen.src = experiencia.imagen;
+        imagen.alt = `Descripción de la imagen de ${experiencia.titulo}`;
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = experiencia.titulo;
+
+        card.appendChild(titulo);
+        card.appendChild(imagen);
+        experienciasContainer.appendChild(card);
+    });
+});
+
+
+// Muestra u oculta el botón de flecha para arriba según la posición de desplazamiento
+window.onscroll = function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+// Función para desplazarse hacia arriba al hacer clic en el botón
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
